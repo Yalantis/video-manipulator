7e9851e85ed332222a582e271b28e7b0108f1bb1
Alexey Suhoviy <alexey.suhoviy@gmail.com>
2017-06-29 17:24:52 +0300

    Add multiple effects selection

--- app/models/video.rb
+++ app/models/video.rb
@@ -9,7 +9,7 @@ class Video
   field :file_duration, type: Integer
   field :progress, type: Float, default: 0
 
-  field :effect, type: String
+  field :effects, type: Array, default: []
 
   mount_uploader :file, ::VideoUploader
   process_in_background :file
@@ -19,17 +19,28 @@ class Video
   mount_uploader :watermark_image, ::ImageUploader
 
   validates :title, presence: true
-  validates :effect, inclusion: { in: ::VideoUploader::ALLOWED_EFFECTS }
+  validate :effects_allowed_check
 
   # This is not good idea to let this thing update database field many times
   # It is made just to test how it works
   def processing_progress(format, format_options, new_progress)
+    ::Rails.logger.info(format_options)
     # Update this value only each 10th percent
     diff = (new_progress.to_f - self.progress.to_f)
     if diff >= 0.1 || (new_progress.to_i == 1)
       # Had to use atomic set operation here since normal update
       # has been setting file_processing to false while processing still was not finished
       self.set(progress: new_progress.to_f)
+    end
+  end
+
+  private
+
+  def effects_allowed_check
+    self.effects.each do |effect|
+      unless ::VideoUploader::ALLOWED_EFFECTS.include?(effect)
+        self.errors.add(:effects, :not_allowed, effect: effect.humanize)
+      end
     end
   end
 
--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -31,7 +31,6 @@ class VideoUploader < CarrierWave::Uploader::Base
   }
 
   EFFECT_PARAMS = {
-    no_effect: [],
     sepia:
       %w(-filter_complex colorchannelmixer=.393:.769:.189:0:.349:.686:.168:0:.272:.534:.131 -c:a copy),
     black_and_white: %w(-vf hue=s=0 -c:a copy),
@@ -44,7 +43,10 @@ class VideoUploader < CarrierWave::Uploader::Base
     reverse: %w(-vf reverse -af areverse),
     slow_down: %w(-filter:v setpts=2.0*PTS -filter:a atempo=0.5),
     speed_up: %w(-filter:v setpts=0.5*PTS -filter:a atempo=2.0),
-    echo: %w(-map 0 -c:v copy -af aecho=0.8:0.9:1000:0.3)
+    echo: %w(-map 0 -c:v copy -af aecho=0.8:0.9:1000|500|500:0.7|0.5|0.3),
+    tremolo: %w(-map 0 -c:v copy -af tremolo=f=10.0:d=0.7),
+    vibrato: %w(-map 0 -c:v copy -af vibrato=f=7.0:d=0.5),
+    chorus: %w(-map 0 -c:v copy -af chorus=0.5:0.9:50|60|40:0.4|0.32|0.3:0.25|0.4|0.3:2|2.3|1.3)
   }.freeze
 
   ALLOWED_EFFECTS = EFFECT_PARAMS.keys.map(&:to_s).freeze
@@ -53,16 +55,16 @@ class VideoUploader < CarrierWave::Uploader::Base
 
   def encode(format, opts={})
     # Normalize file format
-    encode_video(format, opts.merge(processing_step: 'normalize'))
+    encode_video(format, opts.merge(processing_metadata: { step: 'normalize' }))
     # Apply effects
-    if model.effect != 'no_effect'
-      encode_video(format, ADDITIONAL_OPTIONS.merge(processing_step: 'apply_effect')) do |_, params|
-        params[:custom] = EFFECT_PARAMS[model.effect.to_sym]
+    model.effects.each do |effect|
+      encode_video(format, ADDITIONAL_OPTIONS.merge(processing_metadata: {step: 'apply_effect', effect: effect })) do |_, params|
+        params[:custom] = EFFECT_PARAMS[effect.to_sym]
       end
     end
     # Apply watermark
     if model.watermark_image.path.present?
-      encode_video(format, ADDITIONAL_OPTIONS.merge(processing_step: 'apply_watermark')) do |_, params|
+      encode_video(format, ADDITIONAL_OPTIONS.merge(processing_metadata: {step: 'apply_watermark' })) do |_, params|
         params[:watermark] ||= {}
         params[:watermark][:path] = model.watermark_image.path
       end
